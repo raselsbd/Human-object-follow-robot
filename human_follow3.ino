389a0e33f9a75bd462c64e74f9f8a9e8d3f6b90b
raselsbd <154895630+raselsbd@users.noreply.github.com>
2025-06-25 23:10:39 +0600

    Update human_follow3.ino

--- human_follow3.ino
+++ human_follow3.ino
@@ -1,3 +1,4 @@
+
 #include<NewPing.h>
 #include<Servo.h>
 #include<AFMotor.h>
@@ -6,13 +7,20 @@
 #define TRIGGER_PIN A1
 #define ECHO_PIN A0
 #define MAX_DISTANCE 100
- NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-AF_DCMotor Motor1(3,MOTOR12_1KHZ);
-AF_DCMotor Motor2(5,MOTOR12_1KHZ);
-AF_DCMotor Motor3(6,MOTOR34_1KHZ);
-AF_DCMotor Motor4(9,MOTOR34_1KHZ);
+  
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+
+AF_DCMotor Motor1(1,MOTOR12_1KHZ);
+AF_DCMotor Motor2(2,MOTOR12_1KHZ);
+AF_DCMotor Motor3(3,MOTOR34_1KHZ);
+AF_DCMotor Motor4(4,MOTOR34_1KHZ);
+
 Servo myservo;
+ 
 int pos =0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -31,45 +39,52 @@ for(pos = 90; pos <= 180; pos += 1){
 }
 pinMode(RIGHT, INPUT);
 pinMode(LEFT, INPUT);
+
 }
+
 void loop() {
   // put your main code here, to run repeatedly:
+
   delay(50);
  unsigned int distance = sonar.ping_cm();
 Serial.print("distance");
 Serial.println(distance);
+
+
 int Right_Value = digitalRead(RIGHT);
 int Left_Value = digitalRead(LEFT);
+
 Serial.print("RIGHT");
 Serial.println(Right_Value);
 Serial.print("LEFT");
 Serial.println(Left_Value);
-if((Right_Value==0) && (distance>=4 && distance<=30)&&(Left_Value==0)){
-  Motor1.setSpeed(120);
-  Motor1.run(BACKWARD);
-  Motor2.setSpeed(120);
-  Motor2.run(BACKWARD);
-  Motor3.setSpeed(120);
-  Motor3.run(BACKWARD);
-  Motor4.setSpeed(120);
-  Motor4.run(BACKWARD);
-}else if((Right_Value==0) && (Left_Value==1)) {
-  Motor1.setSpeed(200);
+
+if((Right_Value==0) && (distance>=5 && distance<=20)&&(Left_Value==0)){
+  Motor1.setSpeed(250);
   Motor1.run(FORWARD);
-  Motor2.setSpeed(200);
+  Motor2.setSpeed(250);
   Motor2.run(FORWARD);
-  Motor3.setSpeed(120);
+  Motor3.setSpeed(250);
+  Motor3.run(FORWARD);
+  Motor4.setSpeed(250);
+  Motor4.run(FORWARD);
+}else if((Right_Value==0) && (Left_Value==1)) {
+  Motor1.setSpeed(250);
+  Motor1.run(FORWARD);
+  Motor2.setSpeed(250);
+  Motor2.run(FORWARD);
+  Motor3.setSpeed(150);
   Motor3.run(BACKWARD);
-  Motor4.setSpeed(120);
+  Motor4.setSpeed(150);
   Motor4.run(BACKWARD);
 }else if((Right_Value==1)&&(Left_Value==0)) {
-  Motor1.setSpeed(120);
+  Motor1.setSpeed(150);
   Motor1.run(BACKWARD);
-  Motor2.setSpeed(120);
+  Motor2.setSpeed(150);
   Motor2.run(BACKWARD);
-  Motor3.setSpeed(120);
+  Motor3.setSpeed(250);
   Motor3.run(FORWARD);
-  Motor4.setSpeed(120);
+  Motor4.setSpeed(250);
   Motor4.run(FORWARD);
 }else if((Right_Value==1)&&(Left_Value==1)) {
   Motor1.setSpeed(0);
@@ -80,7 +95,7 @@ if((Right_Value==0) && (distance>=4 && distance<=30)&&(Left_Value==0)){
   Motor3.run(RELEASE);
   Motor4.setSpeed(0);
   Motor4.run(RELEASE);
-}else if(distance > 1 && distance < 3) {
+}else if(distance > 1 && distance < 3 ){
   Motor1.setSpeed(0);
   Motor1.run(RELEASE);
   Motor2.setSpeed(0);
@@ -90,4 +105,5 @@ if((Right_Value==0) && (distance>=4 && distance<=30)&&(Left_Value==0)){
   Motor4.setSpeed(0);
   Motor4.run(RELEASE);
   }
- } 
+}
+ 
